8122212dde5246ab53210915746bca032c8de3fc
irza <khafid@thegreatsoft.com>
2024-05-10 07:45:12 +0700

    first commit

---
+++ src/operatorsarithmetic/OperatorsArithmetic.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operatorsarithmetic;
+
+/**
+ *
+ * @author khafi
+ */
+public class OperatorsArithmetic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 20;
+        int b = 10;
+        
+        System.out.println("Penggunaan Operator Aritmatika");
+        System.out.println("Nilai awal a adalah : "+a);
+        System.out.println("Nilai awal b adalah : "+b);
+        System.out.println("Hasil dari a + b = "+(a + b));
+        System.out.println("Hasil dari a - b = "+(a - b));
+        System.out.println("Hasil dari a / b = "+(a / b));
+        System.out.println("Hasil dari a * b = "+(a * b));
+        System.out.println("Hasil dari a % b = "+(a % b));
+    }
+    
+}
